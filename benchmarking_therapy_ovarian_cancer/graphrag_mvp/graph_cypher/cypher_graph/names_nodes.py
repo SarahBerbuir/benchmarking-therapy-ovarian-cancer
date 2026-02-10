81a8d015dc3138a234f6f1665cfd635a24dd12c1
ge83cak <berbuir.sa@gmail.com>
2026-02-10 02:17:26 +0100

    Saved cypher name parameter in a python data structure

---
+++ benchmarking_therapy_ovarian_cancer/graphrag_mvp/graph_cypher/cypher_graph/names_nodes.py
@@ -0,0 +1,74 @@
+names = {
+    "intake": "Vorsorge/Symptome",
+    "sonography": "Gynäkologische Untersuchung & Sonographie",
+    "iota": "IOTA-Auswertung (Simple Rules)",
+    "cystClassification": "Zystenklassifikation (BD-Klassifikation)",
+    "ct": "CT Thorax/Abdomen",
+    "figoBucketer": "FIGO Bucketer",
+    "debulkingAss": "Debulking Assessment",
+    "opDecider": "OP-Entscheider",
+    "followUp": "Verlaufskontrolle",
+    "cystectomy": "Zystenausschälung",
+    "adnexectomy": "Adnektomie",
+    "resectabilityEval": "Beurteilung Resektabilität im Tumorboard",
+    "laparotomy": "Laparotomie, SS", # Changes need to be also done in evidence pass
+    "laparotomyPathology": "Laparotomie, Pathologie Auswertung",
+    "laparoscopy": "Laparoskopie oder Minilaparotomie", # Changes need to be also done in evidence pass
+    "laparoscopyPathology": "Laparoskopie, Pathologie Auswertung",
+    "interdTb": "Interdisziplinäres Tumorboard",
+
+    "geneticCounselingGermlineBrca": "Humangenetische Beratung (gBRCA)",
+    "tumorHRDTesting": "HRD Testung (sBRCA & sHRD)",
+    "hrcBrcaResolver": "HRD/BRCA Resolver",
+
+    "neoadjuvantTherapyMapping": "Neoadjuvante Therapie Mapping",
+    "neoadjuvantTherapy": "Neoadjuvante Therapie",
+    "nextStepMappingNeoAdj": "Next Step Mapping Neoadjuvant",
+    "interimRestagingCycle3": "Reevaluation nach 3 Zyklen, CT Thorax Abdomen, CA125, Operabilität Assessment",
+    "chemoProtocolSwitchAndCompletion": "Wechsel Therapieprotokoll & Komplementierung Chemotherapie (3x)",
+    "optimalDebulkingAndChemoCompletion": "Optimales Debulking & Komplementierung Chemotherapie (3x)",
+    "repeatDebulkingAssessment": "Wiederholung Debulking Assessment",
+    "therapyReevaluation": "Therapie Reevaluation",
+
+    "adjuvantTherapyMapping": "Adjuvante Therapie Mapping",
+    "adjuvantTherapy": "Adjuvante Therapie",
+    "nextStepMappingAdj": "Next Step Mapping Adjuvant",
+
+    "parallelJoin": "Parallel Join",
+
+    "maintenanceTherapyMapping": "Erhaltungs Therapie Mapping",
+    "maintenanceTherapy": "Erhaltungs Therapie",
+
+    "followUpCare": "Nachsorge",
+
+    "routeFollowUpOp": "Route Verlaufskontrolle (op_plan)",
+    "routeFollowUpCystectomy": "Route Verlaufskontrolle (Histologie Zystenausschälung)",
+    "routeAdnexectomyOpPlan": "Route Adnektomie (op_plan Adnexectomy)",
+    "routeFollowUpAdnex": "Route Verlaufskontrolle (Histologie Adnektomie)",
+
+    "routeFollowUpLaparotomy": "Route Verlaufskontrolle (Laparotomie)",
+    "routeFollowUpLaparoscopy": "Route Verlaufskontrolle (Laparoskopie)",
+
+    "routeAdnexectomyCFigo": "Route Adnektomie (cFIGO early)",
+    "routeDebulkingAssFIGOBucket": "Route Debulking Assessment (FIGO Bucket)",
+    "routeDebulkingAssCystectomy": "Route Debulking Assessment (Zystenausschälung)",
+    "routeDebulkingAssAdnexectomy": "Route Debulking Assessment (Adnektomie)",
+
+    "routeInterdTbLaparotomy": "Route Interdisziplinäres Tumorboard (Laparotomie)",
+    "routeInterdTbLaparoscopy": "Route Interdisziplinäres Tumorboard (Laparoskopie)",
+
+    "routeBrcaHrdResolverHRDTesting": "Route HRD/BRCA Resolver (HRD Testung)",
+    "routeBrcaHrdResolverGermline": "Route HRD/BRCA Resolver (Humangenetische Beratung gBRCA1/2)",
+
+    "routeOptimalDebulkingComplRestaging": "Route Optimales Debulking & Komplementtierung Chemotherapie (3x) (Interim Restaging)",
+    "routeOptimalDebulkingComplRepeatDebAss": "Route Optimales Debulking & Komplementtierung Chemotherapie (3x) (Repeated Debulking Assessment)",
+
+    "routeNeoadjuvantNextStep": "Route Next Step (Neoadjuvant)",
+    "routeSystemDoneNeoadj": "Route Systemtherapie Done (Neoaduvant)",
+
+    "routeAdjuvantNextStep": "Route Next Step (Adjuvant)",
+    "routeSystemDoneAdj": "Route Systemtherapie Done (Adjuvant)",
+
+    "routeFollowUpCareSystTh": "Route Nachsorge (Systemtherapie)",
+    "routeFollowUpCareMaintTh": "Route Nachsorge (Erhaltungstherapie)"
+}
